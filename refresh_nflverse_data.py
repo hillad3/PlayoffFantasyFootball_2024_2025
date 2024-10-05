37be227d0e0e1250551b5c6faa0c6e35bec3b435
hillad3 <adam.x.hill@gmail.com>
2024-10-04 22:11:27 -0400

    Refactored to include timestamps logic to determine if data files get updated.

--- refresh_nflverse_data.py
+++ refresh_nflverse_data.py
@@ -7,7 +7,7 @@ import os
 import pandas as pd
 import logging
 from typing import List
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %I:%M %p')
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.WARNING, datefmt='%Y-%m-%d %I:%M %p')
 
 ## create lists of repo timestamps and raw urls
 # %%
@@ -41,12 +41,12 @@ def get_nflverse_timestamp(url: str) -> str:
   timestamp_url = url_path + 'timestamp.txt?raw=true'
   req = get(timestamp_url)
   if req.status_code == 200:
-    date_time = req.text.replace("\n","")
+    date_time = req.text.replace("\n","").replace(":","")
     logging.info("nflverse asset timestamp: " + date_time + "\n")
     return date_time
   else:
     print(f"Failed to retrieve the timestamp.txt file. Status code: {req.status_code}")
-    return '9999-99-99 99:99:99 EST' # return dummy that will always result in the data file being updated
+    return '9999-99-99 999999 EST' # return dummy that will always result in the data file being updated
 
 # %%
 def parse_nflverse_filename(url: str) -> str:
@@ -58,6 +58,16 @@ def parse_nflverse_filename(url: str) -> str:
   github_file_string = github_file_string.replace(r"?raw=true","")
   github_file_string = github_file_string.split(r'.') 
   return github_file_string[0], github_file_string[1]
+
+# %%
+def parse_local_filename(file_name: str) -> str:
+  """
+  Parses timestamp from local file in data_path directory.
+  """
+  timestamp = file_name[file_name.rfind(',')+2:]
+  timestamp = timestamp[:timestamp.rfind(r'.')]
+  logging.info("Local timestamp: " + timestamp)
+  return timestamp
 
 # %%
 def make_new_file_name(url : str) -> str:
@@ -72,68 +82,70 @@ def make_new_file_name(url : str) -> str:
   return new_data_file_name
 
 # %%
+def remove_old_file(file_name : str) -> None:
+  """
+  Delete the file name. Called from is_new_nflverse_file when True.
+  """
+  logging.warning('Removing local file: ' + file_name)
+  os.remove(data_path + file_name)
+
+
+# %%
 def is_new_nflverse_file(url: str) -> bool:
   """
   The timestamp.txt github URL for the nflverse data file is fetched and compared against
   the timestamp in the local data file name. The local file is determine based on a substring 
-  match parsed from the github URL being requested. This function returns multiple items: 
-  First item is boolean to determine course of action in update_nflverse_data function. Second item
-  is the local file name that will be deleted if there is a new timestamp.txt file. Third item
-  is the new file name that will be written. If this function returns false, dummy parameters are 
-  returned.
+  match parsed from the github URL being requested. If matched, delete the old file.
   """
-  github_timestamp = get_nflverse_timestamp(url)
-  
+
   github_file_name = parse_nflverse_filename(url)[0]
 
   data_files = os.listdir(data_path)
-  logging.info("##### Directory Files #####")
-  for i in data_files:
-    logging.info(i)
 
-
-  logging.info("\n")
   for d in data_files:
     logging.info("Local file: " + d)
     logging.info("Target substring: " + github_file_name)
-    logging.info("Substring test: " + str(github_file_name in d))
+    logging.info("Substring match test: " + str(github_file_name in d))
     if github_file_name in d:
-      local_file_to_delete = d
-      logging.info("Match found. Returning actionable parameters.")
-      return True, local_file_to_delete, make_new_file_name(url)
-
-  logging.info("No match found. Returning dummy parameters.")  
-  return False, "dummy.parquet", "dummy.parquet"
+      local_timestamp = parse_local_filename(d)
+      github_timestamp = get_nflverse_timestamp(url)
+      logging.info("Github file name substring matches an existing file. Evaluating timestamps...")
+      if local_timestamp != github_timestamp:
+        logging.info("Timestamps indicate a new data file.")
+        remove_old_file(d)
+        return True
+      else:
+        logging.info("Timestamps indicate no change to data file.")
+        return False
+         
+  logging.info("No substring match found with files in data_path.")  
+  return False
 
 # %%
 def update_nflverse_data(url : str, force_update : bool = False) -> None:
   """
-  The URL for the data file is evalauted to determine which local
-  file is to be updated if the timestamp.txt file indicates a different 
-  timestamp in the local file name. If updating, delete the old data file
-  and write the new data file. Currently using *.parquet files to keep
-  requests and data storage smaller but in theory this function is 
-  agnostic to file type (.csv and .rds are available)
+  The URL for the data file is evalauted to determine which Github
+  file is to be downloaded, if any (based on timestamp comparison to local 
+  file). Currently using *.parquet files to keep requests and data storage 
+  smaller but in theory this function is agnostic to file type (.csv 
+  and .rds are available)
   """
 
-  perform_update, local_file_to_remove, new_data_file_name = is_new_nflverse_file(url)
+  perform_update = is_new_nflverse_file(url)
 
   if perform_update:
-      logging.warning('Removing local file: ' + local_file_to_remove + '\n')
-      os.remove(data_path + local_file_to_remove)
+      new_data_file_name = make_new_file_name(url)
       logging.warning('Writing new local file: ' + new_data_file_name + '\n')
       df = pd.read_parquet(url, engine = 'pyarrow')
       df.to_parquet(data_path + new_data_file_name, engine='pyarrow')
   elif force_update:
-      github_file_name, github_file_type = parse_nflverse_filename(url)
-      github_timestamp = get_nflverse_timestamp(url)
-      new_data_file_name_forced = github_file_name + ", " + github_timestamp.replace(":","") + "." + github_file_type
+      new_data_file_name_forced = make_new_file_name(url)
       logging.warning('Writing new local file (forced): ' + new_data_file_name + '\n')
       df = pd.read_parquet(url, engine = 'pyarrow')
       df.to_parquet(data_path + new_data_file_name_forced, engine='pyarrow')
-      logging.warning('No file cleanup performed. Any old data files should be deleted.')  
+      logging.warning('No file cleanup performed. Any old data files should be deleted manually.')  
   elif not perform_update and not force_update:
-      logging.warning('Local file is up-to-date. No action performedd.')
+      logging.warning('Local file is up-to-date. No action performed.')
   else:
       logging.warning('No case matched to determine if update is needed.')
 
